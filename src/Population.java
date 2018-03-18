67106c8b1e0b7a8c66f34e7ea75aead73b3e5506
Gerald Glasgow <geraldjglasgow@gmail.com>
2018-03-18 01:06:02 -0400

    - chromosomes are sorted from least to greatest, then negative values are put at end of array

--- src/Population.java
+++ src/Population.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Population {
 
     private Chromosome[] chromosomes;
@@ -8,14 +10,46 @@ public class Population {
     }
 
     public void sortChromosomesByFitness(){
-
+        Arrays.sort(chromosomes, (chromosomes1, chromosomes2) -> {
+            int flag = 0;
+            if(chromosomes1.getFitness() > chromosomes2.getFitness()) flag = -1;
+            else if(chromosomes1.getFitness() < chromosomes2.getFitness()) flag = 1;
+            return flag;
+        });
+        boolean sorted = false;
+        while(!sorted && chromosomes.length == 8) {
+            sorted = true;
+            for (int i = 0; i < 7; i++) {
+                if (chromosomes[i].getFitness() > chromosomes[i + 1].getFitness() && chromosomes[i].getFitness() > 0 && chromosomes[i + 1].getFitness() > 0) {
+                    Chromosome ch = chromosomes[i];
+                    chromosomes[i] = chromosomes[i + 1];
+                    chromosomes[i + 1] = ch;
+                    sorted = false;
+                }
+            }
+        }
+        while(!sorted && chromosomes.length == 4) {
+            sorted = true;
+            for (int i = 0; i < 3; i++) {
+                if (chromosomes[i].getFitness() > chromosomes[i + 1].getFitness() && chromosomes[i].getFitness() > 0 && chromosomes[i + 1].getFitness() > 0) {
+                    Chromosome ch = chromosomes[i];
+                    chromosomes[i] = chromosomes[i + 1];
+                    chromosomes[i + 1] = ch;
+                    sorted = false;
+                }
+            }
+        }
     }
 
-    public Population initializePopulation(){
+    public Population initializePopulation(double length){
         for(int a=0;a<chromosomes.length;a++){
-            chromosomes[a] = new Chromosome(SIZE).initializeChromosome();
+            chromosomes[a] = new Chromosome(SIZE, length).initializeChromosome();
         }
         sortChromosomesByFitness();
         return this;
+    }
+
+    public Chromosome[] getChromosomes(){
+        return chromosomes;
     }
 }
