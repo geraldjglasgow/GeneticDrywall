61eca3add240685498cfa40df429760f1c10778c
Gerald Glasgow <geraldjglasgow@gmail.com>
2018-03-18 01:00:42 -0400

    - finished fitness function

--- src/Chromosome.java
+++ src/Chromosome.java
@@ -2,35 +2,48 @@ import java.util.Random;
 import java.util.Arrays;
 
 public class Chromosome {
-    private int fitness = 0;
+    private double fitness = 0;
+    boolean isFitnessChanged = true;
     private int[] genes;
     private final int MAX = 10;
+    double length = 0;
     Random rand = new Random();
 
-    public Chromosome(int length){
+    public Chromosome(int length,double len){
         genes = new int[length];
+        this.length = len;
     }
 
     public Chromosome initializeChromosome() {
+
         for(int a=0; a<genes.length;a++){
            genes[a] = rand.nextInt(MAX - 0 + 1);
         }
         return this;
     }
 
-    public int recalculateFitness(){
-
+    public double recalculateFitness(){
+        return calculateSum()-length;
     }
 
     public int[] getGenes(){
+        isFitnessChanged = true;
         return genes;
     }
 
-    public int getFitness(){
+    public double getFitness(){
+        if(isFitnessChanged){
+            fitness = recalculateFitness();
+            isFitnessChanged = false;
+        }
         return fitness;
     }
 
     public String toString(){
         return Arrays.toString(this.genes);
+    }
+
+    public double calculateSum(){
+        return (genes[0]*8) + (genes[1]*10) + (genes[2]*12) + (genes[3]*14) + (genes[4]*16);
     }
 }
